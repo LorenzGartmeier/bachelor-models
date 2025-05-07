4ac04b85b05fd4c27874cbd43188115d59acf273
Lorenz Gartmeier <logartmeier@gmail.com>
2025-05-07 14:14:58 +0200

    add method for resized data loading

--- dataloading/loadResized.py
+++ dataloading/loadResized.py
@@ -3,10 +3,11 @@ import os
 import keras
 from keras import datasets
 
-def getDatasetFromDirectory(path, batch_size) -> tf.data.Dataset:
-    return keras.utils.image_dataset_from_directory(directory = path,
-                                                       labels = None,
-                                                       color_mode = "grayscale",
-                                                       batch_size = batch_size
-                                                       )
-    
+def getDatasetFromDirectory(path, batch_size, resize_height = 255, resize_width = 255) -> tf.data.Dataset:
+    return keras.utils.image_dataset_from_directory(
+        directory=path,
+        labels= None,
+        color_mode="grayscale",
+        image_size=(resize_height, resize_width),
+        batch_size=batch_size
+    )
