67c4cb459e6b9639e522be16cdacce44bb37bfe4
Lorenz Gartmeier <logartmeier@gmail.com>
2025-05-07 12:22:54 +0200

    move SceneApproximator from main.py to own file

--- main.py
+++ main.py
@@ -6,12 +6,25 @@ import keras
 import numpy as np
 from keras import Model, layers
 
+from model import SceneContentApproximator
+
 
 resize_height, resize_width = 256, 256
 
 # should be odd numbers
 kernel_height, kernel_width = 11, 11
+num_kernels = 4
+
+
 learning_rate = 0.001
+
+
+# please refer to the paper
+loss_constant_alpha = 0.01
+
+# please refer to the paper 
+loss_constant_lambda = 1.0
+
 
 def getDatasetFromDirectory(path, batch_size) -> tf.data.Dataset:
     return keras.utils.image_dataset_from_directory(
@@ -28,109 +41,47 @@ dataset = dataset.take(512)
 
 iterator = dataset.as_numpy_iterator()
 batch = iterator.next()
-print(batch.shape) 
-print(batch[0].shape)
+
 
 fig, ax = plt.subplots(ncols=5, figsize=(20, 20))
-for idx, img in enumerate(batch[:1]):
+for idx in range(5):
+    img = iterator.next()[0]
     img_display = np.squeeze(img)
     ax[idx].imshow(img_display, cmap='gray') 
     ax[idx].axis('off') 
 
 plt.show()  
-
-
-class KernelConstraint(keras.constraints.Constraint):
-    def __call__(self, w):
-            
-        print("the shape of the kernel is ")
-        print(w.shape)
-
-        w_shape = w.shape
-        middle_zero_tensor = np.ones(w_shape)
-
-        middle_zero_tensor[w_shape[0] // 2, w_shape[1] // 2, :, :] = 0
-
-        
-        return w * middle_zero_tensor
-
-
-
-class SceneContentApproximator(Model):
-
-    def __init__(self):
-        super(SceneContentApproximator, self).__init__()
-        self.conv = layers.Conv2D(1, (kernel_height,kernel_width), use_bias= False, kernel_constraint = KernelConstraint())
-
-    
-    def call(self, input):
-        return self.conv(input)
-    
-    
-    
-    def train(self, image_batch, epochs):
-
-        def custom_loss(y_true, y_pred):
-            # y_true: (resize_height, resize_width, 1)
-            # y_pred: (resize_height - kernel_height + 1, resize_width - kernel_width + 1, 1)
-
-            # cut off edges
-            batch_size = len(y_true)
-            y_true = y_true[:batch_size,kernel_height//2:-kernel_height//2,kernel_height//2:kernel_height/2]
-            return math.reduce_sum(math.square(y_true - y_pred))
-        
-
-        optimizer = keras.optimizers.AdamW(learning_rate)
-
-        @tf.function
-        def train_step(image_batch):
-            with tf.GradientTape() as tape:
-                prediction = self(image_batch, training=True)
-                loss = custom_loss(image_batch, prediction)
-            
-            # Get gradients and update weights
-            gradients = tape.gradient(loss, self.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-        
-        for epoch in range (epochs):
-            train_step(image_batch)
-
-
-
-    def train_and_get(self, image_batch, epochs):
-        self.train(image_batch, epochs)
-        return self.call(image_batch)
         
 
 
 
 batch = iterator.next()
-image = batch[0]
+image = np.squeeze(batch[0])
 
-print('Shape of the batch:')
-print(batch.shape)
+sceneContentApproximator = SceneContentApproximator(num_kernels, kernel_height, kernel_width, learning_rate, loss_constant_alpha, loss_constant_lambda)
 
-sceneContentApproximator = SceneContentApproximator()
-
-prediction_batch = sceneContentApproximator.train_and_get(batch, 10000)
+prediction_batch = sceneContentApproximator.train_and_get(batch, 10)
 
 approximated_image = prediction_batch[0]
 
+residual = np.squeeze(image) - approximated_image[:,:,0]
+
+weights = sceneContentApproximator.get_weights()[0]
+weights_min = tf.reduce_min(weights).numpy()
+weights_max =tf.reduce_max(weights).numpy()
 
 
-fig, ax = plt.subplots(ncols=2, figsize=(20, 20))
-ax[0].imshow(np.squeeze(image), cmap = 'gray')
-ax[0].axis('off')
-ax[1].imshow(np.squeeze(approximated_image), cmap = 'gray')
-ax[1].axis('off')
+fig, ax = plt.subplots(nrows = num_kernels, ncols = 4, figsize=(20, 20))
+for i in range(num_kernels):
+    kernel = np.squeeze(weights[:,:,:,i]) / max(abs(weights_min), abs(weights_max))
+    approximated_image = np.squeeze(prediction_batch[0, :, :, i])
+    residual = image - approximated_image
+    ax[i, 0].imshow(kernel, cmap = 'bwr', vmin = -1, vmax = 1)
+    ax[i, 1].imshow(image, cmap = 'gray')
+    ax[i, 2].imshow(approximated_image, cmap = 'gray')
+    ax[i, 3].imshow(residual, cmap = 'gray')
 
 plt.show()  
-
-print(sceneContentApproximator.get_weights())
-
-
-
-
 
         
 
---
+++ model/SceneContentApproximator.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+from tensorflow import math
+import keras
+import numpy as np
+from keras import Model, layers
+
+
+class SceneContentApproximator(Model):
+
+
+
+    def __init__(self, num_kernels, kernel_height, kernel_width, learning_rate, loss_contsant_alpha, loss_constant_lambda):
+        super(SceneContentApproximator, self).__init__()
+
+
+        self.learning_rate = learning_rate
+        self.conv = layers.Conv2D(num_kernels, (kernel_height,kernel_width), use_bias= False, kernel_regularizer = KernelDiversityLoss(loss_constant_alpha, loss_constant_lambda), kernel_constraint = KernelConstraint(), padding='same')
+
+    
+    def call(self, input):
+        return self.conv(input)
+    
+    
+    
+    def train(self, image_batch, epochs):
+
+        def custom_loss(y_true, y_pred):
+            # y_true.shape (1, resize_height, resize_width, 1)
+            # y_true.shape (1, resize_height, resize_width, num_kernels)
+
+            # y_true gets broadcasted
+            discrepancy_loss =  math.reduce_sum(math.square(y_true - y_pred))
+            return discrepancy_loss
+                                                
+
+
+
+        optimizer = keras.optimizers.AdamW(self.learning_rate)
+
+        @tf.function
+        def train_step(image_batch):
+            with tf.GradientTape() as tape:
+                prediction = self(image_batch, training=True)
+                loss = custom_loss(image_batch, prediction)
+            
+            # Get gradients and update weights
+            gradients = tape.gradient(loss, self.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        
+        for epoch in range (epochs):
+            train_step(image_batch)
+
+
+
+    def train_and_get(self, image_batch, epochs):
+        self.train(image_batch, epochs)
+        return self.call(image_batch)
+    
+
+
+class KernelConstraint(keras.constraints.Constraint):
+
+    def __init__(self, ):
+        super().__init__()
+
+
+    def __call__(self, w):
+            
+        # shape of w: (kernel_height, kernel_width, num_colorchannels (1 in case of grayscaled), num_kernels)
+
+        w_shape = w.shape
+        middle_zero_tensor = np.ones(w_shape)
+
+        middle_zero_tensor[int(w_shape[0]/2), int(w_shape[1]/2), :, :] = 0
+
+        middle_zero_w =  w * middle_zero_tensor
+
+
+        sums = tf.reduce_sum(middle_zero_w, [0,1,2], keepdims = True)   
+        return middle_zero_w / sums
+
+    
+class KernelDiversityLoss(keras.regularizers.Regularizer):
+
+    def __init__(self, loss_constant_alpha, loss_constant_lambda, num_kernels, kernel_height, kernel_width):
+        super().__init__()
+        self.num_kernels = num_kernels
+        self.kernel_height = kernel_height
+        self.kernel_width = kernel_width
+        self.loss_constant_lambda = loss_constant_lambda
+        self.loss_constant_alpha = loss_constant_alpha
+
+
+    def __call__(self, kernel_tensor):
+            # shape: (1, kernel_height, kernel_width, num_kernels)
+
+            # shape (kernel_height * kernel_width, num_kernels)
+            flattened_kernels = tf.squeeze(tf.reshape(kernel_tensor, [1, self.kernel_height * self.kernel_width, self.num_kernels])) 
+
+
+
+            singular_values = tf.linalg.svd(flattened_kernels)
+            return - self.loss_constant_lambda * tf.reduce_sum(math.log(singular_values + self.loss_constant_alpha))
