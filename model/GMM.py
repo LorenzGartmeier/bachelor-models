8a70a0d968ea8078bc695147f61ddcd946fd639e
Lorenz Gartmeier <logartmeier@gmail.com>
2025-05-12 13:25:24 +0200

    write a simple gmm with scikit learn

---
+++ model/GMM.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sklearn
+
+class GMM_EM:
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.model = sklearn.mixture.GaussianMixture(n_components=n_components, covariance_type='full', random_state=42)
+
+    def fit(self, X):
+        # uses the EM algorithm
+        # X (numpy.ndarray): Training data of shape (n_samples, n_features).
+        self.model.fit(X)
+    
+    def get_likelihood(self, X):
+        # X: shape (n_samples, n_features)
+
+        # output: shape (n_samples), log likelihood for each sample
+        return self.model.score_sample(X)
