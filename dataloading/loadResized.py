a7ebe9841e0320a53818f72fdd5a914b1abce9de
Lorenz Gartmeier <logartmeier@gmail.com>
2025-05-15 10:38:31 +0200

    add func for labeled dataset loading

--- dataloading/loadResized.py
+++ dataloading/loadResized.py
@@ -11,3 +11,13 @@ def getDatasetFromDirectory(path, batch_size, resize_height = 255, resize_width 
         image_size=(resize_height, resize_width),
         batch_size=batch_size
     )
+
+def getLabeledDatasetFromDirectory(path, batch_size, resize_height = 255, resize_width = 255) -> tf.data.Dataset:
+    return keras.utils.image_dataset_from_directory(
+        directory=path,
+        labels= "inferred",
+        label_mode="int",
+        color_mode="grayscale",
+        image_size=(resize_height, resize_width),
+        batch_size=batch_size
+    )
