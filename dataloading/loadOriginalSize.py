77ea48f1bfe2cd8d0e4748228621dfb9516f413c
Lorenz Gartmeier <logartmeier@gmail.com>
2025-06-09 22:03:08 +0100

    implement unresized image loading

--- dataloading/loadOriginalSize.py
+++ dataloading/loadOriginalSize.py
@@ -3,12 +3,31 @@ import os
 
 def imagepathToTensor(imagepath) -> tf.Tensor:
     img = tf.io.read_file(imagepath)
-    img = tf.image.decode_image(img)
-    img = tf.image.
+    img = tf.io.decode_image(img)
     img = tf.image.rgb_to_grayscale(img)
-    img = img /255
+
+    # if normalization is desired
+    # img = img /255
 
     return img
 
 
-if __name__ == "__main__":
+def getDatasetFromDirectory(path, batch_size) -> tf.data.Dataset:
+    jpg_ds = tf.data.Dataset.list_files(os.path.join(path, "*/*.jpg"))
+    png_ds = tf.data.Dataset.list_files(os.path.join(path, "*/*.png"))
+    jpeg_ds = tf.data.Dataset.list_files(os.path.join(path, "*/*.jpeg"))
+
+    file_ds = jpg_ds.concatenate(png_ds).concatenate(jpeg_ds)
+    image_ds = file_ds.map(
+    imagepathToTensor,
+    num_parallel_calls=tf.data.AUTOTUNE,
+    deterministic=False
+    )
+
+    padded_ds =  image_ds.padded_batch(
+        batch_size=batch_size,
+        padded_shapes=[None, None, 1],  
+        padding_values=0.0
+    )
+
+    return padded_ds
