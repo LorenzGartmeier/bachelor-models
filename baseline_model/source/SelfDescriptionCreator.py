2c7b3dc80e031bdde88c8cedc8cd3f6e33a7c9c9
Lorenz Gartmeier <logartmeier@gmail.com>
2025-06-26 13:12:05 +0200

    roll back to most performant version yet

--- baseline_model/source/SelfDescriptionCreator.py
+++ baseline_model/source/SelfDescriptionCreator.py
@@ -13,8 +13,6 @@ class SelfDescriptionCreator(Model):
         self.beta = beta
 
         # num kernels is the number of residuals, determined by the prevous SceneContentApproximator
-        self.kernel_height = kernel_height
-        self.kernel_width = kernel_width
         self.num_kernels = num_kernels
         self.learning_rate = learning_rate
 
@@ -22,7 +20,7 @@ class SelfDescriptionCreator(Model):
 
         self.kernel_constraint = KernelConstraint((kernel_height, kernel_width, num_kernels, 1)) 
         # Depthwise convolution (one 11x11 filter per residual channel)
-        self.depthwise_conv = keras.layers.DepthwiseConv2D(
+        self.depthwise_conv = tf.keras.layers.DepthwiseConv2D(
             kernel_size=(kernel_height,  kernel_width),
             depth_multiplier=1,
             padding='same',
@@ -32,32 +30,19 @@ class SelfDescriptionCreator(Model):
         )
 
     @tf.function(jit_compile=True)
-    def _forward(self, resized_list, height, width):
-        total   = tf.zeros([1, height, width], tf.float32)
-
-        resized_list = resized_list.copy()
-        def cond(idx, _):
-            return tf.less(idx, self.L)          
-
-        def body(idx, acc):
-            r   = resized_list.pop()
-            e   = r - self.depthwise_conv(r)
-            acc = acc + tf.reduce_sum(
-                        tf.image.resize(e, (height, width), "bilinear"), axis=-1)
-            return idx + 1, acc
-
-        _, total = tf.while_loop(
-            cond, body,
-            loop_vars=[tf.constant(0), total],
-            maximum_iterations=self.L,         
-            parallel_iterations=1)
-
-        return total
+    def _forward(self, x):
+        B, H, W, _ = x[0].shape
+        total = tf.zeros([B, H, W], x[0].dtype)
+        for resized_image in x:
+            e = resized_image - self.depthwise_conv(resized_image)
+            total += tf.reduce_sum(
+                         tf.image.resize(e, (H, W), "bilinear"), axis=-1)
+        return total                                    # H×W error
 
     @tf.function(jit_compile=True)
-    def train_step(self, x, height, width):
+    def train_step(self, x):
         with tf.GradientTape() as tape:
-            err  = self._forward(x, height, width)
+            err  = self._forward(x)
             loss = tf.reduce_mean(tf.square(err))      # MSE
             loss += self.beta * tf.add_n(
                         [tf.nn.l2_loss(v) for v in self.depthwise_conv.weights])
@@ -67,47 +52,35 @@ class SelfDescriptionCreator(Model):
         return loss
     
     # expects a batch with shape (1, image_height, image_width, num_kernels)
-    def train(self, image, height, width, epochs):
+    def train(self, image, epochs):
         resized_list = []
-        l = tf.constant(1)
-        while l < self.L + 1:
+
+        for l in tf.range(1, self.L + 1):
             f = 2 ** (l - 1)
-            resized_image = tf.image.resize(image, (height // f, width // f), "bilinear")
+            resized_image = tf.image.resize(image, (image.shape[1]//f, image.shape[2]//f), "bilinear", antialias=True)
             resized_list.append(resized_image)
-            l += 1
-        
-        i = tf.constant(0)
-        loss = tf.constant(0, dtype= tf.float32)
-        while i < epochs:
-            loss = self.train_step(resized_list, height, width)
-            i += 1
+
+        for _ in tf.range(epochs):
+            loss = self.train_step(resized_list)
+
         return loss
 
     # expects batches from the SceneContentApproximator of shape (batch_size, image_height, image_width, num_kernels)
     # returns a tensor of shape (batch_size, num_weights) and a tensor of shape (batch_size,1) for the losses
     def train_and_get(self, image_batch, epochs):
-        residual_list = tf.unstack(image_batch)
-        residual_list = [tf.expand_dims(x, axis=0) for x in residual_list]
-        batch_size = len(residual_list)
-        tensor_list = tf.TensorArray(dtype = image_batch.dtype, size = batch_size)
-        for i, residual in enumerate(residual_list):
-            tensor_list = tensor_list.write(i, residual)
 
-        selfdescriptions_list = tf.TensorArray(dtype = tf.float32, size = batch_size, element_shape=tf.TensorShape([self.kernel_height * self.kernel_width * self.num_kernels]))
-        loss_list = tf.TensorArray(dtype = tf.float32, size = batch_size)
-
-        i = tf.constant(0)
-        while i < batch_size:
+        selfdescriptions_list = []
+        loss_list = []
+        
+        for i in tf.range(tf.shape(image_batch)[0]):
             self.reset_conv_weights()
-            image = tensor_list.read(i)
-            height, width = tf.shape(image)[1], tf.shape(image)[2]
-            loss = self.train(image, height, width, epochs)
-            flattened_weights = tf.reshape(self.depthwise_conv.kernel, [-1])
-            selfdescriptions_list = selfdescriptions_list.write(i, flattened_weights)
-            loss_list = loss_list.write(i, loss)
-            i += 1
+            loss = self.train(image_batch[i:i+1], epochs)
+            weights = self.get_weights()[0]
+            flattened_weights = tf.reshape(weights, [-1])
+            selfdescriptions_list.append(flattened_weights)
+            loss_list.append(loss)
 
-        return selfdescriptions_list.stack(), loss_list.stack()
+        return tf.stack(selfdescriptions_list, axis=0), tf.stack(loss_list, axis=0)
     
     def reset_conv_weights(self):
         for v in self.depthwise_conv.weights:
@@ -130,8 +103,6 @@ class KernelConstraint(keras.constraints.Constraint):
     def __call__(self, w):
             
         return w * self.mask
-    
-
 
 
     
