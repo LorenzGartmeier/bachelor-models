91efdafef462476934fcaa37ae798f015b8541f5
Lorenz Gartmeier <logartmeier@gmail.com>
2025-06-26 16:45:13 +0200

    remove downlampling and l2 loss, add loss history

--- baseline_model/source/SelfDescriptionCreator.py
+++ baseline_model/source/SelfDescriptionCreator.py
@@ -6,12 +6,9 @@ import numpy as np
 
 
 class SelfDescriptionCreator(Model):
-    def __init__(self, kernel_height, kernel_width, num_kernels, L, learning_rate, beta):
+    def __init__(self, kernel_height, kernel_width, num_kernels, learning_rate):
         super(SelfDescriptionCreator, self).__init__()
         
-        self.L = L
-        self.beta = beta
-
         # num kernels is the number of residuals, determined by the prevous SceneContentApproximator
         self.num_kernels = num_kernels
         self.learning_rate = learning_rate
@@ -30,57 +27,45 @@ class SelfDescriptionCreator(Model):
         )
 
     @tf.function(jit_compile=True)
-    def _forward(self, x):
-        B, H, W, _ = x[0].shape
-        total = tf.zeros([B, H, W], x[0].dtype)
-        for resized_image in x:
-            e = resized_image - self.depthwise_conv(resized_image)
-            total += tf.reduce_sum(
-                         tf.image.resize(e, (H, W), "bilinear"), axis=-1)
-        return total                                    # H×W error
+    def _forward(self, residual):
+        e = residual - self.depthwise_conv(residual)
+        return tf.reduce_sum(e, axis=-1)
 
     @tf.function(jit_compile=True)
     def train_step(self, x):
         with tf.GradientTape() as tape:
             err  = self._forward(x)
-            loss = tf.reduce_mean(tf.square(err))      # MSE
-            loss += self.beta * tf.add_n(
-                        [tf.nn.l2_loss(v) for v in self.depthwise_conv.weights])
+            loss = tf.reduce_mean(tf.square(err))    
         g = tape.gradient(loss, self.trainable_variables)
         g, _ = tf.clip_by_global_norm(g, 10.0)
         self.optimizer.apply_gradients(zip(g, self.trainable_variables))
         return loss
     
     # expects a batch with shape (1, image_height, image_width, num_kernels)
-    def train(self, image, epochs):
-        resized_list = []
-
-        for l in tf.range(1, self.L + 1):
-            f = 2 ** (l - 1)
-            resized_image = tf.image.resize(image, (image.shape[1]//f, image.shape[2]//f), "bilinear", antialias=True)
-            resized_list.append(resized_image)
+    def train(self, residual, epochs):
+        loss_history = []
 
         for _ in tf.range(epochs):
-            loss = self.train_step(resized_list)
+            loss_history.append(self.train_step(residual))
 
-        return loss
+        return loss_history
 
     # expects batches from the SceneContentApproximator of shape (batch_size, image_height, image_width, num_kernels)
     # returns a tensor of shape (batch_size, num_weights) and a tensor of shape (batch_size,1) for the losses
-    def train_and_get(self, image_batch, epochs):
+    def train_and_get(self, residual_batch, epochs):
 
         selfdescriptions_list = []
         loss_list = []
         
-        for i in tf.range(tf.shape(image_batch)[0]):
+        for i in tf.range(tf.shape(residual_batch)[0]):
             self.reset_conv_weights()
-            loss = self.train(image_batch[i:i+1], epochs)
+            loss = self.train(residual_batch[i:i+1], epochs)
             weights = self.get_weights()[0]
             flattened_weights = tf.reshape(weights, [-1])
             selfdescriptions_list.append(flattened_weights)
             loss_list.append(loss)
 
-        return tf.stack(selfdescriptions_list, axis=0), tf.stack(loss_list, axis=0)
+        return tf.stack(selfdescriptions_list, axis=0), loss_list
     
     def reset_conv_weights(self):
         for v in self.depthwise_conv.weights:
