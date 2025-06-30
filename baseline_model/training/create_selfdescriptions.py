64585fb0fc3e10575c2cdfdfc193b8ccfa8ca304
Lorenz Gartmeier <logartmeier@gmail.com>
2025-06-30 20:25:58 +0100

    adapt to new batch size = 1

--- baseline_model/training/create_selfdescriptions.py
+++ baseline_model/training/create_selfdescriptions.py
@@ -8,16 +8,17 @@ sys.path.append(project_root)
 import tensorflow as tf
 from baseline_model.source.SceneContentApproximator import SceneContentApproximator
 from baseline_model.source.SelfDescriptionCreator import SelfDescriptionCreator
-from dataloading.loadCropped import getDatasetFromDirectory
+from dataloading.loadOriginalSize import getDatasetFromDirectory
 import numpy as np
 import time
+import keras
 import pandas as pd
 from matplotlib import pyplot as plt
 
 
-batch_size = 32
+batch_size = 1
 
-sceneContentApproximator = tf.keras.models.load_model(
+sceneContentApproximator = keras.models.load_model(
     'baseline_model/saved_weights/scene_content_approximator.keras',
     custom_objects={'SceneContentApproximator': SceneContentApproximator}
 )
@@ -46,54 +47,34 @@ for folder in folder_list:
         os.makedirs(os.path.join('baseline_model/self_descriptions', folder, 'test'), exist_ok=True)
 
         i = 0
-
+        batch_counter
+        batch_size = 32
+        selfdescriptions_list = []
         start_time = time.time()
         for image_batch in dataset:
-            if os.path.exists(os.path.join('baseline_model/self_descriptions', folder, f'train/batch_{i}.npy')) \
-            or os.path.exists(os.path.join('baseline_model/self_descriptions', folder, f'test/batch_{i}.npy')):
-                i += 1
-                continue
-
-            if i > 500:
-                break
-
+            
 
             approximations = sceneContentApproximator(image_batch)
 
             residuals = image_batch - approximations
 
-            selfdescriptions, loss_list = selfDescriptionCreator.train_and_get(residuals, 4000)
-            current_time = time.time()
-            tf.print(f"Processed batch {i} in {current_time - start_time:.2f} seconds")
-            start_time = current_time
+            selfdescription = selfDescriptionCreator.train_and_get(residuals, 10000)
+            batch_counter += 1
+            selfdescriptions_list.append(selfdescription)
 
+            if batch_counter >= batch_size:
+                selfdescription_batch = tf.stack(selfdescriptions_list, axis=0)
+                selfdescriptions_list = []
+                if np.random.rand() < 0.2:
+                    subfolder = 'test'
+                else:
+                    subfolder = 'train'
+                    np.save(os.path.join('baseline_model/self_descriptions', folder, subfolder, f'batch_{i}.npy'), selfdescription_batch.numpy())
 
-
-                        # Save training history
-            history_df = pd.DataFrame(loss_list)
-            history_df.to_csv(f"baseline_model/training/results/selfdescriptions_loss_batch_{i}.csv", index=False)
-                
-            # Generate and save loss plot
-            plt.figure(figsize=(12, 6))
-            for sample_loss in loss_list:
-                plt.plot(sample_loss, label=None, linewidth=2)
-
-            plt.title("Training Losses", fontsize=14)
-            plt.xlabel("Epoch", fontsize=12)
-            plt.ylabel("Loss", fontsize=12)
-            plt.legend(fontsize=10)
-            plt.grid(alpha=0.3)
-            plt.tight_layout()
-            plt.savefig(f"baseline_model/training/results/selfdescriptions_loss_batch_{i}.png", dpi=300)
-            plt.close()
-
-
-            if np.random.rand() < 0.2:
-                 subfolder = 'test'
-            else:
-                subfolder = 'train'
-            np.save(os.path.join('baseline_model/self_descriptions', folder, subfolder, f'batch_{i}.npy'), selfdescriptions.numpy())
-
-            i += 1
+                current_time = time.time()
+                tf.print(f"Processed batch {i} in {current_time - start_time:.2f} seconds")
+                start_time = current_time
+                i += 1
+                batch_counter = 0
 
             
